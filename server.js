23d8245c3c2d8bd4dd71a569cadddd7fa93812ef
HHolben <hdholben@my.milligan.edu>
2024-07-21 02:42:14 -0400

    Added Box  item for code editor

--- server.js
+++ server.js
@@ -72,14 +72,17 @@ app.get('/api/file', async (req, res) => {
 app.post('/api/save', async (req, res) => {
     const { path: filePath, content } = req.body;
 
-    if (!filePath || !content) {
+    if (!filePath || !content)
+    {
         return res.status(400).send('File path and content are required');
     }
 
-    try {
+    try
+    {
         await fs.writeFile(filePath, content, 'utf8');
         res.send('File saved successfully');
-    } catch (err) {
+    }
+    catch (err) {
         res.status(500).send('Error saving file');
     }
 });
