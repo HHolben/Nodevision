85fe5f473de3186732ec205d7c3dbbf3795b4efc
HHolben <hdholben@my.milligan.edu>
2024-08-18 09:46:11 -0400

    WYSIWYG editor still does not work, but at least you can get to it

--- public/SendToWYSIWYGeditorPage.js
+++ public/SendToWYSIWYGeditorPage.js
@@ -2,7 +2,7 @@
     // Ensure that ActiveNode is set
     if (window.ActiveNode !== undefined) {
         // Construct the URL with the current ActiveNode value
-        const url = `WYSIYGeditor.html?filepath=${encodeURIComponent(window.ActiveNode)}`;
+        const url = `WYSIWYGeditor.html?activeNode=${encodeURIComponent(window.ActiveNode)}`;
         // Redirect to the constructed URL
         window.location.href = url;
     } else {
